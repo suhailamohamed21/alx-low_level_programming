a1a45f23b0500d48021ac251dd8c52ed6ffc16bd
suhaila <suhilamohamed47@gmail.com>
2023-03-23 15:19:43 -0700

    loop

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#inlude "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: size of triangle
+ *
+ * Return: Alwaysa 0
+*/
+
+void print_triangle(int size)
+{
+	int base, height;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (height = 1; height <= size; height++)
+		{
+			for (base = 1; base <= size; base++)
+			{
+				if ((height + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+
+}

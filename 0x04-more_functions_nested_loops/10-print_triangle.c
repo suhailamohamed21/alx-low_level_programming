e2bed20927867d6d66a40fb48b0ebea25193e116
suhaila <suhilamohamed47@gmail.com>
2023-03-23 15:31:23 -0700

    loop

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -20,7 +20,7 @@ void print_triangle(int size)
 		{
 			for (base = 1; base <= size; base++)
 			{
-				if ((height + base) <= size)
+				if ((height + base) < size)
 					_putchar(' ');
 				else
 					_putchar('#');

db6af40135c6ec5d3c76eb7e369c58c79c602811
suhaila <suhilamohamed47@gmail.com>
2023-03-27 18:43:02 -0700

    po

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reversed string
+ *
+ * @s: input string
+ *
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

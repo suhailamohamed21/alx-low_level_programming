acc245fb4ba4e044a33df820c24e7723a3960137
suhaila <suhilamohamed47@gmail.com>
2023-03-27 20:11:25 -0700

    po

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,9 +13,9 @@ void rev_string(char *s)
 	int l, i;
 	char temp;
 
-	for (1 = 0; s[l] != '\0'; ++l)
+	for (1 = 0; s[i] != '\0'; ++l)
 		;
-	i++;
+
 	for (i = 0; i < l / 2; i++)
 	{
 		temp = s[i];

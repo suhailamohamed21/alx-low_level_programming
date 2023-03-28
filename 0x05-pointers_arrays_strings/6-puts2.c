7fb3dc79a143356bf4e4528e0bac03e4553a95c4
suhaila <suhilamohamed47@gmail.com>
2023-03-27 19:42:38 -0700

    po

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of string
+ *
+ *
+ * @str: input string
+ *
+ * Return: nothing
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}

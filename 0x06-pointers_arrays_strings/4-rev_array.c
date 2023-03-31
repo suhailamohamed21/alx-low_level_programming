617b2793656ef055f50b1e7194a9d6d2a975c10f
suhaila <suhilamohamed47@gmail.com>
2023-03-30 18:27:47 -0700

    more

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,10 +12,10 @@ void reverse_array(int *a, int n)
 {
 	int i, j, t;
 
-	for (i = 0, j = (n - 1); i < j; i++; j--)
+	for (i = 0, j = (n - 1); i < j; i++, j--)
 	{
 		t = a[i];
 		a[i] = a[j];
-		a[j] = t;
+		a[j] = ti;
 	}
 }
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - change all lowercase to uppercase
+ *
+ * @str: string to return
+ * Return: string
+*/
+
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
